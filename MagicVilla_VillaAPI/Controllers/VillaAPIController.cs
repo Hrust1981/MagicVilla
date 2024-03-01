68c04d30c21ae20753aa5670d09fdb28f5185f44
Hrust1981 <rustamshar1981@gmail.com>
2024-03-01 22:28:17 +0500

    create the GetVilla method

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,3 +1,4 @@
+using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
@@ -11,10 +12,12 @@ public class VillaAPIController : ControllerBase
     [HttpGet]
     public IEnumerable<VillaDTO> GetVillas()
     {
-        return new List<VillaDTO>
-        {
-            new VillaDTO { Id = 1, Name = "Pool View" },
-            new VillaDTO { Id = 2, Name = "Beach View" }
-        };
+        return VillaStore.villaList;
+    }
+    
+    [HttpGet("{id:int}")]
+    public VillaDTO GetVilla(int id)
+    {
+        return VillaStore.villaList.FirstOrDefault(x => x.Id == id);
     }
 }
